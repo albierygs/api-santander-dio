0185174414967773b23abc413b01fa71a336280d
Albiery Gonçalves dos Santos <137830475+albierygs@users.noreply.github.com>
2023-10-22 12:59:28 -0300

    Add files via upload

---
+++ santander-dio-api/src/test/java/edu/albiery/SantanderDioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.albiery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
